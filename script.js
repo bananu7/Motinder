2bef5e74e508fb879f160effc26688623e70382d
bananu7 <bananu7@o2.pl>
2016-08-23 15:26:22 +0200

    Removed repeated swiping and added db logic

--- script.js
+++ script.js
@@ -2,64 +2,66 @@ $(document).ready(function(){
     console.log('init');
 
     firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {        
-        console.log('signed in as ' + user.uid);
+        if (user) {        
+            console.log('signed in as ' + user.uid);
 
-        firebase.database().ref('pending').on("child_added", function(data) {
-          console.log(data);
-          addLink(data.val());
-        }, function (errorObject) {
-          console.log("The read failed: " + errorObject.code);
-        });
-
-      } else {
-        firebase.auth().signInWithEmailAndPassword('example@example.com', 'example').catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log('auth error: ' + error.message)
-        });
-      }
+            firebase.database().ref('pending').on("child_added", function(data) {
+                console.log(data);
+                addLink(data.val(), data.key);
+            }, function (errorObject) {
+                console.log("The read failed: " + errorObject.code);
+            });
+        } else {
+            firebase.auth().signInWithEmailAndPassword('example@example.com', 'example').catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log('auth error: ' + error.message)
+            });
+        }
     });
-
 });
 
-function addLink(url) {
-  var elem = $('<div></div>');
-  elem.addClass('buddy');
-  elem.css('display', 'block');
+function addLink(url, id) {
+    var elem = $('<div></div>');
+    elem.addClass('buddy');
+    elem.css('display', 'block');
 
-  var img = $('<div></div>');
-  img.addClass('avatar');
-  img.css("display", 'block')
-  img.css('background-image', 'url(' + url + ')');
-  elem.append(img);
+    var img = $('<div></div>');
+    img.addClass('avatar');
+    img.css("display", 'block')
+    img.css('background-image', 'url(' + url + ')');
+    elem.append(img);
 
-  elem.on("swiperight", function(){
-    $(this).addClass('rotate-left').delay(700).fadeOut(1);
-    $('.buddy').find('.status').remove();
+    elem.on("swiperight", function(){
+        if ($(this).hasClass('rotate-left') || 
+            $(this).hasClass('rotate-right')) {
+            return;
+        }
 
-    $(this).append('<div class="status like">Like!</div>');      
-    if ( $(this).is(':last-child') ) {
-      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-     }
-  });
+        $(this).addClass('rotate-left').delay(700).fadeOut(1);
+        $(this).append('<div class="status like">Like!</div>');
 
-  elem.on("swipeleft", function(){
-    $(this).addClass('rotate-right').delay(700).fadeOut(1);
-    $('.buddy').find('.status').remove();
-    $(this).append('<div class="status dislike">Dislike!</div>');
+        firebase.database().ref('pending/' + id).remove();
+        firebase.database().ref('right').push(url);
 
-    if ( $(this).is(':last-child') ) {
-     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-      alert('Na-na!');
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-    }
-  });
+        console.log('swiperight on ' + url);
+    });
 
-  $('#container').append(elem);
+    elem.on("swipeleft", function(){
+        if ($(this).hasClass('rotate-left') || 
+            $(this).hasClass('rotate-right')) {
+            return;
+        }
+
+        $(this).addClass('rotate-right').delay(700).fadeOut(1);
+        $(this).append('<div class="status dislike">Dislike!</div>');
+
+        firebase.database().ref('pending/' + id).remove();
+        firebase.database().ref('left').push(url);
+
+        console.log('swipeleft on ' + url);
+    });
+
+    $('#container').append(elem);
 }
-    
